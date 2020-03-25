72c7be653fff02b5d07a907fb9d881254b7350fd
mengj520 <54372998+mengj520@users.noreply.github.com>
2020-03-25 16:07:28 -0400

    Add files via upload

---
+++ JinlinMengLAB3.R
@@ -0,0 +1,32 @@
+# Lab Tres, Hier Clustering, Heatmaps
+rm(list=ls())
+set.seed(12345)
+#help(par)
+par(mar = rep(0.2,4))
+data_Matrix <-matrix(rnorm(400), nrow = 40)
+image(1:10, 1:40, t(data_Matrix)[,nrow(data_Matrix):1])
+
+par(mar=rep(0.2,4))
+heatmap(data_Matrix)
+
+set.seed(678910)
+for(i in 1:40){
+  # flipping a coin and getting the data
+  coin_Flip <- rbinom(1, size = 1, prob = 0.5)
+  # if the coin is "Heads", add a common pattern to that row,
+  if(coin_Flip){
+    data_Matrix[i, ] <- data_Matrix[i, ] + rep(c(0,3), each =5)
+  }
+}
+par(mar= rep(0.2, 4))
+image(1:10, 1:40, t(data_Matrix)[, nrow(data_Matrix):1])
+par(mar=rep(0.2, 4))
+heatmap(data_Matrix)
+
+
+hh<-hclust(dist(data_Matrix))
+data_Matrix_Ordered<-data_Matrix[hh$order,]
+par(mfrow=c(1,3))
+image(t(data_Matrix_Ordered))[,nrow(data_Matrix_Ordered):1]
+plot(rowMeans(data_Matrix_Ordered),40:1,xlab="the row mean",ylab="row",pch=19)
+plot(colMeans(data_Matrix_Ordered),xlab="column",ylab="the column mean",pch=19)
